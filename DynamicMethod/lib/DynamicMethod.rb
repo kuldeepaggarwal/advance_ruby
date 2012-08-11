27981e1803798a8dc9b776b4b34c71217cae7cf8
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-08-11 21:32:38 +0530

    intendent

--- DynamicMethod/lib/DynamicMethod.rb
+++ DynamicMethod/lib/DynamicMethod.rb
@@ -3,7 +3,7 @@ class DynamicMethod < String
 		@value = value
 	end
   def exclude? substring 
-  	! @value.include?substring
+    !@value.include?substring
   end
   def append ( *args )
     1.upto(args[1].to_i) { @value = args[0] + @value + args[0] }
