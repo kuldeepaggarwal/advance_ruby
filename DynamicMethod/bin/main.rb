3a29cde5969fee811d7195e75481fc1164d65f52
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-08-11 19:44:49 +0530

    Dynmic Method

---
+++ DynamicMethod/bin/main.rb
@@ -0,0 +1,12 @@
+require_relative "../lib/DynamicMethod.rb"
+puts "exclude? StringToFind"
+puts "append AppendChar,times"
+
+print 'Create class object for class "DynamicMethod(\'value\')": '
+input_object = gets.chomp
+object = eval input_object
+print 'Enter function name and its parameters: '
+input_fn = gets.chomp
+object.instance_eval {
+	puts eval input_fn
+}
---
+++ DynamicMethod/lib/DynamicMethod.rb
@@ -0,0 +1,17 @@
+class DynamicMethod < String
+	def initialize value
+		@value = value
+	end
+  def exclude? substring 
+  	! @value.include?substring
+  end
+  def append ( *args )
+  	1.upto(args[1].to_i) { @value = args[0] + @value + args[0] }
+  	@value
+  end
+end
+
+if __FILE__ == $0
+	dm = DynamicMethod.new("iti")
+	puts dm.append('N','1')
+end
