5ed6d5300e084230c8e98d8c0f7fa4bf7e629117
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-08-11 21:36:51 +0530

    intendent

--- SingletonMethod/lib/SingletonMethod.rb
+++ SingletonMethod/lib/SingletonMethod.rb
@@ -1,12 +1,12 @@
 animal1 = "cat"
 animal2 = "dog"
- class << animal1
-	def speak 
-		puts "meow"
-	end
+  class << animal1
+    def speak 
+    puts "meow"
+  end
 end
 def animal1.feet 
-	puts "#{self} is of 5 feet"
+  puts "#{self} is of 5 feet"
 end
 animal1.speak
 animal1.feet
