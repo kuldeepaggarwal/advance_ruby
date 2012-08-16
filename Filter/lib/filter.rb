f3b43da92ca5cf41261f5ee67be1e6524c63bea4
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-08-16 13:04:17 +0530

    filter

--- Filter/lib/filter.rb
+++ Filter/lib/filter.rb
@@ -3,34 +3,39 @@ module AddMethods
     cls.instance_eval {
       @@afters, @@befores = {}, {}  
       def before_filter (*before)
-        for_before = filters(before)
-        method_to_be_changed =  for_before[0]
-        method_to_be_added = for_before[1]
+        method_to_be_changed,method_to_be_added = filters(before)
         method_to_be_changed.each do |fname|
           @@befores[fname] = method_to_be_added - [fname]
         end
         if ( @@afters.length != 0 )
           remove_infinte_problem
-          create
+          override_existing_methods
         end
       end
       def after_filter (*after)
-        for_after = filters(after)
-        method_to_be_changed =  for_after[0]
-        method_to_be_added = for_after[1]
+        method_to_be_changed,method_to_be_added = filters(after)
         method_to_be_changed.each do |fname|
           @@afters[fname] = method_to_be_added - [fname]
         end
         if ( @@befores.length != 0 )
           remove_infinte_problem
-          create
+          override_existing_methods
         end
       end
-      def create
+      def filters(*args)
+        if args[0].last.class != Hash
+          return self.instance_methods - Object.instance_methods - args[0] , args[0]
+        elsif args[0].last[:only]
+          return (args[0].last[:only].to_a) , (args[0] - [args[0].last])
+        else
+          return self.instance_methods - Object.instance_methods - args[0] - args[0].last[:except] , (args[0] - [args[0].last])
+        end
+      end
+      def override_existing_methods
         all = self.public_instance_methods - Object.methods
           all.each do |variable|
             current_method = instance_method(variable)
-            define_method "#{variable}" do
+            define_method variable do
               if(@@befores[variable].class != NilClass)
                 @@befores[variable].each do | b |
                   send(b)
@@ -50,13 +55,13 @@ module AddMethods
           value.each do |key_for_before|
             if(@@befores[key_for_before].class != NilClass)
             indx = @@befores[key_for_before].index(key)
-            if ( indx.class == Fixnum  )
+            if (indx.class == Fixnum)
               @@befores[key_for_before].delete(key)        
             end
           end
           if(@@afters[key_for_before].class != NilClass)
             indx_self =  @@afters[key_for_before].index(key)
-            if ( indx_self.class == Fixnum  )
+            if (indx_self.class == Fixnum)
               @@afters[key_for_before].delete(key)        
             end
           end
@@ -66,20 +71,11 @@ module AddMethods
           value.each do |key_for_before|
             if(@@befores[key_for_before].class != NilClass)
               indx_self =  @@befores[key_for_before].index(key)
-              if ( indx_self.class == Fixnum  )
+              if (indx_self.class == Fixnum)
                 @@befores[key_for_before].delete(key)        
               end
             end
           end
-        end
-      end
-      def filters(*args)
-        if args[0].last.class != Hash
-          [ self.instance_methods - Object.instance_methods - args[0] ,args[0] ]
-        elsif args[0].last[:only]
-          return [(args[0].last[:only].to_a) , (args[0] - [args[0].last])]
-        else
-          [self.instance_methods - Object.instance_methods - args[0] - args[0].last[:except], (args[0] - [args[0].last])]
         end
       end
     }
@@ -102,7 +98,7 @@ class Methods
     puts "in test method"
   end
   include AddMethods
-  after_filter :foo,:method_name1, :except => [:test_method]
+  after_filter :foo,:method_name1
   before_filter :bar,:method_name2
  
 end
