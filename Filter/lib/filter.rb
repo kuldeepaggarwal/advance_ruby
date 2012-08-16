2aa08fdab67cef258a4a6079fb5915e569c62c46
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-08-16 12:36:36 +0530

    filter

--- Filter/lib/filter.rb
+++ Filter/lib/filter.rb
@@ -1,92 +1,89 @@
 module AddMethods
   def self.included(cls)
     cls.instance_eval {
-    @@s = cls
-  @@afters, @@befores = {}, {}  
-  def self.before_filter (*before)
-    for_before = filters(before)
-    method_to_be_changed =  for_before[0]
-    method_to_be_added = for_before[1]
-    method_to_be_changed.each do |fname|
-      @@befores[fname] = method_to_be_added - [fname]
-    end
-    if ( @@afters.length != 0 )
-       remove_infinte_problem
-       create
-    end
-  end
-  def create
-    
-    all = self.public_instance_methods - Object.methods
-      all.each do |variable|
-        a = instance_method(variable)
-        define_method "#{variable}" do
-          if(@@befores[variable].class != NilClass)
-            @@befores[variable].each do | b |
-              send(b)
+      @@afters, @@befores = {}, {}  
+      def self.before_filter (*before)
+        for_before = filters(before)
+        method_to_be_changed =  for_before[0]
+        method_to_be_added = for_before[1]
+        method_to_be_changed.each do |fname|
+          @@befores[fname] = method_to_be_added - [fname]
+        end
+        if ( @@afters.length != 0 )
+          remove_infinte_problem
+          create
+        end
+      end
+      def create
+        all = self.public_instance_methods - Object.methods
+          all.each do |variable|
+            current_method = instance_method(variable)
+            define_method "#{variable}" do
+              if(@@befores[variable].class != NilClass)
+                @@befores[variable].each do | b |
+                  send(b)
+                end
+              end
+              current_method.bind(self).()          
+              if(@@afters[variable].class != NilClass)
+                @@afters[variable].each do | a |
+                  send(a)
+                end
+              end        
             end
           end
-          a.bind(self).()          
-          if(@@afters[variable].class != NilClass)
-            @@afters[variable].each do | a |
-              send(a)
+      end
+      def after_filter (*after)
+        for_after = filters(after)
+        method_to_be_changed =  for_after[0]
+        method_to_be_added = for_after[1]
+        method_to_be_changed.each do |fname|
+          @@afters[fname] = method_to_be_added - [fname]
+        end
+        if ( @@befores.length != 0 )
+          remove_infinte_problem
+          create
+        end
+      end
+      def remove_infinte_problem
+        @@afters.each do |key,value|
+          value.each do |key_for_before|
+            if(@@befores[key_for_before].class != NilClass)
+            indx = @@befores[key_for_before].index(key)
+            if ( indx.class == Fixnum  )
+              @@befores[key_for_before].delete(key)        
             end
           end
-        
+          if(@@afters[key_for_before].class != NilClass)
+            indx_self =  @@afters[key_for_before].index(key)
+            if ( indx_self.class == Fixnum  )
+              @@afters[key_for_before].delete(key)        
+            end
+          end
+        end
+        end
+        @@befores.each do |key,value|
+          value.each do |key_for_before|
+            if(@@befores[key_for_before].class != NilClass)
+              indx_self =  @@befores[key_for_before].index(key)
+              if ( indx_self.class == Fixnum  )
+                @@befores[key_for_before].delete(key)        
+              end
+            end
+          end
         end
       end
-  end
-  def after_filter (*after)
-    for_after = filters(after)
-    method_to_be_changed =  for_after[0]
-    method_to_be_added = for_after[1]
-    method_to_be_changed.each do |fname|
-      @@afters[fname] = method_to_be_added - [fname]
-    end
-    if ( @@befores.length != 0 )
-       remove_infinte_problem
-       create
-    end
-  end
-  def remove_infinte_problem
-    @@afters.each do |key,value|
-      value.each do |key_for_before|
-        if(@@befores[key_for_before].class != NilClass)
-        indx = @@befores[key_for_before].index(key)
-        if ( indx.class == Fixnum  )
-          @@befores[key_for_before].delete(key)        
+      def filters(*args)
+        if args[0].last.class != Hash
+          [ self.instance_methods - Object.instance_methods - args[0] ,args[0] ]
+        elsif args[0].last[:only]
+          return [(args[0].last[:only].to_a) , (args[0] - [args[0].last])]
+        else
+          [self.instance_methods - Object.instance_methods - args[0] - args[0].last[:except], (args[0] - [args[0].last])]
         end
       end
-      if(@@afters[key_for_before].class != NilClass)
-        indx_self =  @@afters[key_for_before].index(key)
-        if ( indx_self.class == Fixnum  )
-          @@afters[key_for_before].delete(key)        
-        end
-      end
-    end
-    end
-    @@befores.each do |key,value|
-      value.each do |key_for_before|
-        if(@@befores[key_for_before].class != NilClass)
-        indx_self =  @@befores[key_for_before].index(key)
-        if ( indx_self.class == Fixnum  )
-          @@befores[key_for_before].delete(key)        
-        end
-      end
-      end
-    end
+    }
   end
-  def filters(*args)
-    if args[0].last.class != Hash
-      [ self.instance_methods - Object.instance_methods - args[0] ,args[0] ]
-    elsif args[0].last[:only]
-      return [(args[0].last[:only].to_a) , (args[0] - [args[0].last])]
-    else
-      [self.instance_methods - Object.instance_methods - args[0] - args[0].last[:except], (args[0] - [args[0].last])]
-    end
-  end
-}
-end
 end
 class Methods
   def method_name1
