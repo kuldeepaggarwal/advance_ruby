512c7bbb20c0e37f8c14ac96b6edbc69ffff633c
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-08-22 11:07:46 +0530

    DSL

--- DSL/bin/main.rb
+++ DSL/bin/main.rb
@@ -5,4 +5,5 @@ sc.item { |i| i.name("Laptop"); i.quantity(3) }
 sc.item { |i| i.name("Desktop"); i.quantity(4) }
 sc.item { |i| i.name("Chairs"); i.quantity(35) }
 sc.item { |i| i.name("Keyboards"); i.quantity(40) }
+sc.item { |i| i.name("Chairs"); i.quantity(35) }
 puts sc
--- DSL/lib/shopping_cart.rb
+++ DSL/lib/shopping_cart.rb
@@ -1,6 +1,6 @@
 class ShoppingCart
   def initialize
-    @items = []
+    @items = {}
     @name = ""
     @quantity = 0
   end
@@ -11,16 +11,25 @@ class ShoppingCart
     @quantity = qty
   end
   def to_s
-    @items.join("\n")
+    items = []
+    @items.each do |key,value|
+      items <<  key + " has " + value.to_s + " quantities\n"
+    end
+    items.join('')
   end
   def item
     yield self
-    @items << @name + ":" + @quantity.to_s + " quantities"
+    if @items.include? @name
+       @items[@name] += @quantity
+    else
+      @items[@name] = @quantity
+    end
   end 
 end
 if __FILE__ == $0
 sc = ShoppingCart.new
 sc.item { |i| i.name("Toothpaste"); i.quantity(3) }
 sc.item { |i| i.name("Computer"); i.quantity(1) }
+sc.item { |i| i.name("Computer"); i.quantity(5) }
 puts sc
 end
